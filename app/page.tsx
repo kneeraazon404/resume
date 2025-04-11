3098026570c25d6e19b315fa8de5ffae7bb558c8
kneeraazon404 <kneeraazon@gmail.com>
2025-04-12 00:00:34 +0545

    initial steps creation

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
 
   return (
     <div className="max-w-5xl mx-auto px-16 py-2 bg-white text-gray-900 shadow-lg rounded-xl space-y-[2px] print:space-y-[2px] print:py-0 print:px-[6px] print:shadow-none print:rounded-none print:bg-white print:text-black text-[12pt] font-normal leading-snug">
-      <div className="flex justify-end print:hidden">
+      <div className="flex justify-end print:hidden mt-8">
         <button
           onClick={handlePrint}
           className="mb-3 px-4 py-1 border border-gray-300 rounded text-sm font-semibold hover:bg-gray-100"
